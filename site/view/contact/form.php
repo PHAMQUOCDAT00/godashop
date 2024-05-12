f21ec75b8fdef53496b438ee7f39b5c802fdd1fa
PHAMQUOCDAT00 <dat.pq.cntt@gmail.com>
2024-05-12 15:29:58 +0700

    init

---
+++ site/view/contact/form.php
@@ -0,0 +1,63 @@
+<?php require ABSPATH_SITE . 'layout/header.php'?>
+<main id="maincontent" class="page-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <ol class="breadcrumb">
+                    <li><a href="/" target="_self">Trang chủ</a></li>
+                    <li><span>/</span></li>
+                    <li class="active"><span>Liên hệ</span></li>
+                </ol>
+            </div>
+        </div>
+        <div class="row contact">
+            <div class="col-md-6">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3898.7063016443044!2d109.19980097490611!3d12.268143687986315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317067ed3a052f11%3A0xd464ee0a6e53e8b7!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBOaGEgVHJhbmc!5e0!3m2!1svi!2s!4v1712216495748!5m2!1svi!2s"
+                    width="580" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-md-6">
+                <h4>Thông tin liên hệ</h4>
+                <form class="form-contact" action="#" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="fullname" placeholder="Họ và tên" required
+                            oninvalid="this.setCustomValidity('Vui lòng nhập tên của bạn')"
+                            oninput="this.setCustomValidity('')">
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <input type="email" class="form-control" name="email" placeholder="Email" required
+                                oninvalid="this.setCustomValidity('Vui lòng nhập email')"
+                                oninput="this.setCustomValidity('')">
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <input type="tel" class="form-control" name="mobile" placeholder="Số điện thoại" required
+                                pattern="[0][0-9]{9,}"
+                                oninvalid="this.setCustomValidity('Vui lòng nhập số điện thoại bắt đầu bằng số 0 và ít nhất 9 con số theo sau')"
+                                oninput="this.setCustomValidity('')">
+                        </div>
+
+                        <div class="form-group col-sm-12">
+
+                            <textarea class="form-control" placeholder="Nội dung" name="content" rows="10"
+                                required></textarea>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+
+                            <!-- .message.alert.alert-success -->
+                            <div class="message alert alert-success" style="display:none"></div>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+                            <button type="submit" class="btn btn-sm btn-primary pull-right">Gửi</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+<?php require ABSPATH_SITE . 'layout/footer.php'?>
